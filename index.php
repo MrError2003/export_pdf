3f9e57a38d42f76004ece5f99ac6628316f372f9
JuanZapata <jdzapatamar@cesde.net>
2025-02-03 01:55:32 -0500

    Se publican algunos cambios

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Button</title>
+</head>
+<body>
+    <?php
+    function testFunction() {
+        echo "Button clicked!";
+    }
+
+    if(isset($_POST['test_button'])) {
+        testFunction();
+    }
+    ?>
+
+    <form action="exportdompdf.php" method="POST">
+        <input type="submit" name="export_button" value="Export to PDF">
+    </form>
+
+    <form action="carta_propietarios.php" method="POST">
+        <input type="submit" name="carta_propietarios" value="CARTA PROPIETARIOS">
+    </form>
+
+    <form action="carta_arrendatarios.php" method="POST">
+        <input type="submit" name="carta_arrendatarios" value="CARTA ARRENDATARIOS">
+    </form>
+</body>
+</html>
